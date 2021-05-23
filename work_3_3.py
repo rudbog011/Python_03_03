1fbcc2ebd61edaa35be9ca1af2b09cc0950eba8f
rudbog011 <rudbog011@gmail.com>
2021-05-23 16:07:42 +0300

    Pyton_input_3_3

---
+++ work_3_3.py
@@ -0,0 +1,8 @@
+print('Как тебя зовут?')
+name = input()
+print('Как ваша фамилия?')
+surname = input()
+print('Как ваше отчество?')
+patronymic = input()
+print(name, '-', surname, '-', patronymic, sep='')
+
